b12a443a6dfab8ceb7fb22a66d60068706ca0019
Alex Mandelias <alexmandelias@gmail.com>
2022-07-07 15:31:57 +0300

    BTM loads BrokerTopics when created and better Exceptions

--- src/eventDeliverySystem/server/BrokerTopicManager.java
+++ src/eventDeliverySystem/server/BrokerTopicManager.java
@@ -1,20 +1,29 @@
 package eventDeliverySystem.server;
 
+import eventDeliverySystem.datastructures.AbstractTopic;
 import eventDeliverySystem.filesystem.FileSystemException;
 import eventDeliverySystem.filesystem.TopicFileSystem;
+import eventDeliverySystem.util.LG;
 
 import java.io.ObjectOutputStream;
 import java.nio.file.Path;
 import java.util.*;
 
-public class BrokerTopicManager implements AutoCloseable {
+public class BrokerTopicManager implements AutoCloseable, Iterable<BrokerTopic> {
 
     private final TopicFileSystem tfs;
     private final Map<String, Set<ObjectOutputStream>> consumerOOSPerTopic = new HashMap<>();
     private final Map<String, BrokerTopic>             topicsByName = new HashMap<>();
 
     public BrokerTopicManager(Path topicsRootDirectory) throws FileSystemException {
+        LG.sout("BrokerTopicManager(%s)", topicsRootDirectory);
+        LG.in();
         tfs = new TopicFileSystem(topicsRootDirectory);
+        for (AbstractTopic abstractTopic : tfs.readAllTopics()) {
+            LG.sout("abstractTopic=%s", abstractTopic);
+            addExistingTopic(new BrokerTopic(abstractTopic, tfs));
+        }
+        LG.out();
     }
 
     @Override
@@ -24,6 +33,11 @@ public class BrokerTopicManager implements AutoCloseable {
                 consumerOOS.close();
     }
 
+    @Override
+    public Iterator<BrokerTopic> iterator() {
+        return topicsByName.values().iterator();
+    }
+
     public boolean topicExists(String topicName) {
         synchronized (topicsByName) {
             return topicsByName.containsKey(topicName);
@@ -31,16 +45,38 @@ public class BrokerTopicManager implements AutoCloseable {
     }
 
     public BrokerTopic getTopic(String topicName) throws NoSuchElementException {
-       if (!topicExists(topicName))
-           throw new NoSuchElementException("There is no Topic with name " + topicName);
+        assertTopicExists(topicName);
 
         synchronized (topicsByName) {
            return topicsByName.get(topicName);
        }
     }
 
-    public void addTopic(String topicName) throws FileSystemException {
+    public void addTopic(String topicName) throws FileSystemException, IllegalArgumentException {
+        assertTopicDoesNotExist(topicName);
+
         BrokerTopic topic = new BrokerTopic(topicName, tfs);
+
+        addExistingTopic(topic);
+
+        synchronized (tfs) {
+            tfs.createTopic(topicName);
+        }
+    }
+
+    public void registerConsumer(String topicName, ObjectOutputStream oos) throws NoSuchElementException {
+        assertTopicExists(topicName);
+
+        synchronized (consumerOOSPerTopic) {
+            consumerOOSPerTopic.get(topicName).add(oos);
+        }
+    }
+
+    private void addExistingTopic(BrokerTopic topic) throws IllegalArgumentException {
+
+        String topicName = topic.getName();
+
+        assertTopicDoesNotExist(topicName);
 
         synchronized (topicsByName) {
             topicsByName.put(topicName, topic);
@@ -49,15 +85,15 @@ public class BrokerTopicManager implements AutoCloseable {
         synchronized (consumerOOSPerTopic) {
             consumerOOSPerTopic.put(topicName, new HashSet<>());
         }
-
-        synchronized (tfs) {
-            tfs.createTopic(topicName);
-        }
     }
 
-    public void registerConsumer(String topicName, ObjectOutputStream oos) {
-        synchronized (consumerOOSPerTopic) {
-            consumerOOSPerTopic.get(topicName).add(oos);
-        }
+    private void assertTopicExists(String topicName) throws NoSuchElementException {
+        if (!topicExists(topicName))
+            throw new NoSuchElementException("No Topic with name '" + topicName + "' found");
+    }
+
+    private void assertTopicDoesNotExist(String topicName) throws IllegalArgumentException {
+        if (topicExists(topicName))
+            throw new IllegalArgumentException("Topic with name '" + topicName + "' already exists");
     }
 }
