03cf77267408936bd7f7d72c895b48a52f4643f8
Alex Mandelias <alexmandelias@gmail.com>
2022-07-08 17:45:43 +0300

    cleans up CrappyUserUI implementation

--- src/app/CrappyUserUI.java
+++ src/app/CrappyUserUI.java
@@ -1,11 +1,11 @@
 package app;
 
-import java.awt.GridLayout;
+import java.awt.*;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -17,14 +17,13 @@ import eventDeliverySystem.User;
 import eventDeliverySystem.datastructures.Post;
 
 /**
- * Imitates the functioanlity of the android app.
+ * Imitates the functionality of the android app.
  *
  * @author Alex Mandelias
  */
-@SuppressWarnings("serial")
 public class CrappyUserUI extends JFrame {
 
-	private final User user;
+	private final transient User user;
 
 	// TODO: remove
 	public class UserUISub {
@@ -47,114 +46,111 @@ public class CrappyUserUI extends JFrame {
 		setSize(800, 600);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-		final JPanel main = new JPanel(new GridLayout(2, 2));
-		main.add(getPostPanel());
-		main.add(getCreateTopicPanel());
-		main.add(getPullPanel());
-		main.add(getListenForTopicPanel());
+		JPanel main = createMain();
+		addPostPanel(main);
+		addOtherPanel(main);
 		add(main);
+
+		this.pack();
 	}
 
-	private final JPanel getPostPanel() {
-		final JPanel main = new JPanel();
-		main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
+	private JPanel createMain() {
+		return new JPanel(new FlowLayout(FlowLayout.LEFT, 30, 0));
+	}
 
-		final JTextField file_text = new JTextField(20);
-		final JTextField topicName = new JTextField(20);
-		final JButton    postFile  = new JButton("File");
-		final JButton    postText  = new JButton("Text");
+	private void addPostPanel(JPanel origin) {
+		final JPanel main = createSection();
 
-		postFile.addActionListener(e -> {
-			try {
-				final User   user1      = CrappyUserUI.this.user;
-				final File   file       = new File(file_text.getText());
-				final String posterName = user1.getCurrentProfile().getName();
-				final Post   post       = Post.fromFile(file, posterName);
-				user1.post(post, topicName.getText());
-			} catch (final IOException e1) {
-				e1.printStackTrace();
-			}
-		});
+		JTextField[] jtfs = new JTextField[2];
+		addTextFieldsToJPanel(main, jtfs);
 
-		postText.addActionListener(e -> {
+		JButton[] buttons = new JButton[2];
+		createButton("File", e -> {
 			final User   user1      = CrappyUserUI.this.user;
-			final String text       = file_text.getText();
+			final File   file       = new File(jtfs[0].getText());
+			final String posterName = user1.getCurrentProfile().getName();
+			tryPST(() -> {
+				Post post = Post.fromFile(file, posterName);
+				user1.post(post, jtfs[1].getText());
+			});
+		}, buttons, 0);
+
+		createButton("Text", e -> {
+			final User   user1      = CrappyUserUI.this.user;
+			final String text       = jtfs[0].getText();
 			final String posterName = user1.getCurrentProfile().getName();
 			final Post   post       = Post.fromText(text, posterName);
-			user1.post(post, topicName.getText());
-		});
+			user1.post(post, jtfs[1].getText());
+		}, buttons, 1);
 
-		main.add(file_text);
-		main.add(topicName);
-		main.add(postFile);
-		main.add(postText);
+		addButtonsToJPanel(main, buttons);
 
-		return main;
+		origin.add(main);
+		// return main; // return the newly created JPanel
 	}
 
-	private final JPanel getCreateTopicPanel() {
-		final JPanel main = new JPanel();
-		main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
+	private void addOtherPanel(JPanel origin) {
+		final JPanel main = createSection();
 
-		final JTextField topicName   = new JTextField(20);
-		final JButton    createTopic = new JButton("Create");
+		JTextField[] jtfs = new JTextField[1];
+		addTextFieldsToJPanel(main, jtfs);
 
-		createTopic.addActionListener(e -> {
-			final User user1 = CrappyUserUI.this.user;
-			try {
-				user1.createTopic(topicName.getText());
-			} catch (final IOException e1) {
-				e1.printStackTrace();
-			}
-		});
+		JButton[] buttons = new JButton[4];
+		createButton("Create", e -> tryPST(() -> user.createTopic(jtfs[0].getText()))
+				, buttons, 0);
+		createButton("Delete", e -> tryPST(() -> user.deleteTopic(jtfs[0].getText()))
+				, buttons, 1);
+		createButton("Pull", e -> tryPST(() -> user.pull(jtfs[0].getText()))
+				, buttons, 2);
+		createButton("Listen", e -> tryPST(() -> user.listenForNewTopic(jtfs[0].getText()))
+				, buttons, 3);
 
-		main.add(topicName);
-		main.add(createTopic);
+		addButtonsToJPanel(main, buttons);
 
-		return main;
+		origin.add(main);
+		// return main; // return the newly created JPanel
 	}
 
-	private final JPanel getPullPanel() {
-		final JPanel main = new JPanel();
-		main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
-
-		final JTextField topicName = new JTextField(20);
-		final JButton    pull      = new JButton("Pull");
-
-		pull.addActionListener(e -> {
-			final User user1 = CrappyUserUI.this.user;
-			try {
-				user1.pull(topicName.getText());
-			} catch (final IOException e1) {
-				e1.printStackTrace();
-			}
-		});
-
-		main.add(topicName);
-		main.add(pull);
-
-		return main;
+	private JPanel createSection() {
+		return new JPanel(new BorderLayout(15, 15));
 	}
 
-	private final JPanel getListenForTopicPanel() {
-		final JPanel main = new JPanel();
-		main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
+	private void addTextFieldsToJPanel(JPanel origin, JTextField[] jtfs) {
 
-		final JTextField topicName      = new JTextField(20);
-		final JButton    lsitenForTopic = new JButton("Listen For Topic");
+		JPanel main = new JPanel(new GridLayout(jtfs.length, 1, 0, 10));
 
-		lsitenForTopic.addActionListener(e -> {
-			final User user1 = CrappyUserUI.this.user;
-			try {
-				user1.listenForNewTopic(topicName.getText());
-			} catch (final IOException e1) {
-				e1.printStackTrace();
-			}
-		});
+		for (int i = 0; i < jtfs.length; i++) {
+			jtfs[i] = new JTextField(15);
+			main.add(jtfs[i]);
+		}
 
-		main.add(topicName);
-		main.add(lsitenForTopic);
+		origin.add(main, BorderLayout.CENTER);
+		// return main; // return the newly created JPanel
+	}
 
-		return main;
+	private void addButtonsToJPanel(JPanel origin, JButton... buttons) {
+		JPanel main = new JPanel(new FlowLayout(FlowLayout.CENTER, 15, 0));
+		for (final JButton button : buttons) {
+			main.add(button);
+		}
+
+		origin.add(main, BorderLayout.SOUTH);
+		// return main; // return the newly created JPanel
+	}
+
+	private void createButton(String text, ActionListener l, JButton[] array, int index) {
+		JButton button = new JButton(text);
+		button.addActionListener(l);
+		array[index] = button;
+	}
+
+	private interface ExceptionRunnable { void run() throws IOException; }
+
+	private void tryPST(ExceptionRunnable r) {
+		try {
+			r.run();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
